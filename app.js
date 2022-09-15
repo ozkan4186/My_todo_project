b3eacbb211ff0e5f7b0166f59536f64ccf3c14f8
özkan demir <ozkanndemir2009@gmail.com>
2022-09-16 02:24:35 +0300

    added file

--- app.js
+++ app.js
@@ -1,18 +1,32 @@
 const icon = document.querySelector("#icon")
 const label = document.querySelector(".form-check-label")
 const count=document.querySelector("#count")
+const card=document.querySelector(".card")
+const zero=document.querySelector(".zero")
+let score=0;
+card.addEventListener("click",(e)=>{
+    console.log(e.target);
+    if(e.target.classList.contains("icon")){
+        
 
-icon.addEventListener("click",(e)=>{
-  
+
+    
        if (e.target.parentElement.classList.contains("check")) {
+        console.log(e.target.childElement);
         e.target.parentElement.classList.remove("check")
-        icon.classList.remove("fa-solid", "fa-check")
-            icon.classList.add("fa-regular", "fa-square")
+        e.target.classList.remove("fa-solid", "fa-check")
+        e.target.classList.add("fa-regular", "fa-square")
+        score --;
+        zero.innerHTML = `${score}`
 
 
        }else{
         e.target.parentElement.classList.add("check")
-        icon.classList.remove("fa-regular" , "fa-square")
-        icon.classList.add("fa-solid", "fa-check")
+    e.target.classList.remove("fa-regular" , "fa-square")
+     e.target.classList.add("fa-solid", "fa-check")
+      score++;
+      zero.innerHTML = `${score} `
+       }
+       
        }
       })
